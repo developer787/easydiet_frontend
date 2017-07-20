84fa9a82071b1bb604b9b8f34bca44ccdba16b3b
Nodejs Puerto Rico <nodejs787@gmail.com>
2017-07-20 02:10:16 +0000

    more components

--- src/components/NewCustomerForm.js
+++ src/components/NewCustomerForm.js
@@ -1,65 +1,151 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import SubmitButton from './SubmitButton'
+import Checkbox from './Checkbox'
 
-class NewCustomerForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-    this.handleClick = this.handleClick.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleInputChange = this.handleInputChange.bind(this)
-  }
-  handleClick(event){
-    console.log('Help!')
-  }
-  handleChange(event){
-    console.log('Change!')
-    const newState = {}
-    newState.name = event.target.value
-    this.setState(newState)
-  }
-  handleSubmit(event){
-    console.log('Submit!')
-    event.preventDefault()
-  }
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+const items = [
+'Carne',
+	'Pollo',
+	'Pescado',
+	'Camarones',
+	'Cebolla',
+	'Nueces'
+	]
+	const Alergias = []
+	class NewCustomerForm extends React.Component {
+		constructor(props) {
+			super(props)
+				this.state = {}
+			this.handleClick = this.handleClick.bind(this)
+				this.handleChange = this.handleChange.bind(this)
+				this.handleSubmit = this.handleSubmit.bind(this)
+				this.handleInputChange = this.handleInputChange.bind(this)
+		}
+		handleClick(event){
+			console.log('Help!')
+		}
+		handleChange(event){
+			console.log('Change!')
+				if(event.target.name === 'nombre'){
+					console.log(1)
+						const target = event.target
+						const value = target.value
+						const name = target.name
+						this.setState({
+								[name]: value
+								})
+				} else {
+					console.log(2)
+						const target = event.target
+						const value = target.value
+						const name = target.name
+						this.setState({
+								[name]: value
+								})
+				}
 
-    this.setState({
-      [name]: value
-    });
-  }
-  render(){
-    return (
-      <div>
-      <form onSubmit={this.handleSubmit}>
-      <label>
-      Name:
-      <input 
-      type="text" 
-      value={this.state.value} 
-      onChange={this.handleChange} />
-      </label>
-      <br />
-      <label>
-      Is going:
-      <input
-      name="isGoing"
-      type="checkbox"
-      checked={this.state.isGoing}
-      onChange={this.handleInputChange} />
-      </label>
-      <SubmitButton handleClick={this.handleClick}/>
-      </form>
-      <p>
-      My name is {this.state.name}
-      </p>
-      </div>
-    )
-  }
+		}
+		handleSubmit(event){
+			console.log('Submit!')
+				event.preventDefault()
+				const self = this
+				const url = 'https://easydiet-backend-developer787.c9users.io/users'
+				fetch(url)
+				.then(function(response) {
+						if (response.status >= 400) {
+						throw new Error("Bad response from server");
+						}
+						return response.json();
+						})
+			.then(function(data) {
+					self.setState({ saludo: data.body });
+					});
+		}
+		handleInputChange(event) {
+			const target = event.target;
+			const value = target.type === 'checkbox' ? target.checked : target.value;
+			const name = target.name;
+
+			this.setState({
+					[name]: value
+					});
+		}
+		componentWillMount = () => {
+			this.selectedCheckboxes = new Set();
+		}
+
+		toggleCheckbox = label => {
+			if (this.selectedCheckboxes.has(label)) {
+				this.selectedCheckboxes.delete(label)
+					const index = Alergias.indexOf(label,0)
+					if (index > -1) {
+						Alergias.splice(index, 1)
+							this.setState({alergias: Alergias})
+					}
+			} else {
+				this.selectedCheckboxes.add(label);
+				Alergias.push(label)
+					this.setState({alergias: Alergias})
+			}
+		}
+		createCheckbox = label => (
+				<Checkbox
+				label={label}
+				handleCheckboxChange={this.toggleCheckbox}
+				key={label}
+				/>
+				)
+			createCheckboxes = () => (
+					items.map(this.createCheckbox)
+					)
+			incluirAlergia = alergia =>
+			<div key={alergia}>
+			{alergia}
+		</div>
+			listarAlergias = () => (
+					Alergias.map(this.incluirAlergia)
+					)
+			render(){
+				return (
+						<div>
+						<form onSubmit={this.handleSubmit}>
+						<label>
+						Nombre:
+						<input 
+						name="nombre"
+						type="text" 
+						value={this.state.nombre || ''} 
+						onChange={this.handleChange} />
+						</label>
+						<label>
+						Apellido:
+						<input 
+						name="apellido"
+						type="text" 
+						value={this.state.apellido || ''}
+						onChange={this.handleChange} />
+						</label>
+						<br />
+						<br />
+						{this.createCheckboxes()}
+						<SubmitButton handleClick={this.handleClick}/>
+							<p>Data: {this.state.saludo}</p>
+							</form>
+							<p>
+							Hola, {this.state.nombre} {this.state.apellido}
+						</p>
+						{this.listarAlergias()}
+						</div>
+							)
+			}
+	}
+NewCustomerForm.propTypes = { 
+nombre: PropTypes.string.isRequired,
+				apellido: PropTypes.string.isRequired
+}
+NewCustomerForm.defaultProps = {
+nombre: "nombre",
+				apellido: "apellido"
 }
 
 export default NewCustomerForm
