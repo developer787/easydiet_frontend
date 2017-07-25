c7a4c0d4692bbea43db8b5d238f0c839f166af07
Nodejs Puerto Rico <nodejs787@gmail.com>
2017-07-25 23:46:27 +0000

    added sub navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react'
  import { Link } from 'react-router-dom'
 import './Header.css'
-const items = ['/', '/listado', '/platos', '/reportes']
+const items = ['/', '/platos', '/reportes']
+const subLinks = ['/listado', '/clientenuevo']
 
 const createNav = (label, i) => {
     let test = 'test'
     switch(label){
       case '/':
         test = 'Clientes'
-        break;
-      case '/listado':
-        test = 'Listado'
         break;
       case '/platos':
         test = 'Platos'
@@ -25,10 +23,32 @@ const createNav = (label, i) => {
     </Link>
   )
   }
+const createSub = (label, i) => {
+    let test = 'test'
+    switch(label){
+      case '/listado':
+        test = 'Ver Todos'
+        break;
+      default:
+        test = 'Cliente Nuevo'
+        break;
+    }
+  return (
+    <Link className={'link'} key={i} to={label}>
+      <span style={{ 
+        marginLeft: '10px',
+        marginRight: '10px'}} className={'link'} >{test}</span>
+    </Link>
+  )
+  }
 const createNavbar = () => (
     items.map(createNav)
 )
+const createSubnav = () => (
+    subLinks.map(createSub)
+)
 const Header = ()=>
+  <div>
   <div className='header'>
    <strong>Easy Diet PR Meal Generator</strong>
    <br />
@@ -36,5 +56,7 @@ const Header = ()=>
    <div className='navBar'>
    {createNavbar()}
    </div>
+  </div>
+   <div className='subnav'>{createSubnav()}</div>
   </div>
 export default Header
--- src/components/NewPlatoForm.js
+++ src/components/NewPlatoForm.js
@@ -1,9 +1,183 @@
 import React from 'react'
-const Platos = () => {
-  return (
-    <div>
-    Platos...
-    </div>
+import './NewCustomerForm.css'
+import SubmitButton from './SubmitButton'
+import Checkbox from './Checkbox'
+import ObjectID from 'bson-objectid'
+
+const items = [
+  'Carne',
+  'Pollo',
+  'Pescado',
+  'Camarones',
+  'Cebolla',
+  'Nueces'
+]
+class NewCustomerForm extends React.Component {
+  constructor(props) {
+
+    const time = Date.now()
+    const id = ObjectID.generate(time)
+    super(props)
+    this.state = {id : id, alergias: [], formStatus: 'default', ul: 'aligned', response: {}}
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleClick(event){
+    console.log('Help!')
+  }
+  handleChange(event){
+    console.log('Change!')
+    if(event.target.name === 'nombre'){
+      console.log(1)
+      const target = event.target
+      const value = target.value
+      const name = target.name
+      this.setState({
+        [name]: value
+      })
+    } else {
+      console.log(2)
+      const target = event.target
+      const value = target.value
+      const name = target.name
+      this.setState({
+        [name]: value
+      })
+    }
+
+  }
+  handleSubmit(event){
+    console.log(this.state.id)
+    event.preventDefault()
+    const self = this
+    const url = 'https://easydiet-backend-developer787.c9users.io/api'
+    const {id, nombre, apellido, alergias } = self.state
+    const payload = {id, nombre, apellido, alergias }
+    const urlOpts = {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    }
+    console.log(payload)
+    fetch(url, urlOpts)
+    .then(function(response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      self.setState({ 
+        response: data,
+        formStatus: 'saved'});
+    });
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+
+  toggleCheckbox = label => {
+    const index = this.state.alergias.indexOf(label.toLowerCase(),0)
+    const Alergias = this.state.alergias
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label)
+      if (index > -1) {
+        Alergias.splice(index, 1)
+        this.setState({alergias: Alergias})
+      }
+    } else {
+      this.selectedCheckboxes.add(label);
+      Alergias.push(label.toLowerCase())
+      this.setState({alergias: Alergias})
+    }
+  }
+  createCheckbox = label => (
+    <Checkbox
+    label={label}
+    handleCheckboxChange={this.toggleCheckbox}
+    key={label}
+    />
   )
+  createCheckboxes = () => (
+    items.map(this.createCheckbox)
+  )
+  incluirAlergia = alergia =>
+  <li key={alergia}>
+  {alergia}
+  </li>
+  listarAlergias = () => {
+    const Alergias = this.state.alergias
+    return  Alergias.map(this.incluirAlergia)
+  }
+  render(){
+    const message = this.state.response.message ? this.state.response.message : '*verificar datos antes de guardar.';
+
+    let p = null;
+    if (message.charAt(0) === '*') {
+      p = <p></p>;
+    } else {
+      p = <p>{message}</p>;
+    }
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+      <div className={this.state.formStatus}>
+      <label>
+      Nombre Del Plato:
+        <input 
+      name="nombre"
+      type="text" 
+      value={this.state.nombre || ''} 
+      onChange={this.handleChange} />
+      </label>
+      </div>
+      <div className={this.state.formStatus}>
+      <p className='aligned'>
+      Seleccione todos los ingredientes que contiene la receta de este plato.
+        </p>
+      <hr />
+      {this.createCheckboxes()}
+      </div>
+      <div className={this.state.formStatus}>
+      <p>
+      Informacion a guardarse
+      </p>
+      <hr />
+      <p className={'aligned'}>
+      Plato: 
+        </p>
+      <strong>
+      {this.state.nombre} {this.state.apellido}
+      </strong>
+      <br />
+      <p className={'aligned'}>
+      Ingredientes:
+        </p>
+      <ul className={this.state.ul}>
+      {this.listarAlergias()}
+      </ul>
+      <br />
+      {p}
+      <SubmitButton handleClick={this.handleClick}/>
+      </div>
+      </form>
+      </div>
+    )
+  }
 }
-export default Platos
+
+export default NewCustomerForm
